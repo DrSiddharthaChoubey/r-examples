3d11f3ac1c49cd780b16a59287d5e8f8b511ef1f
NNK <nnkumar13@gmail.com>
2022-07-31 12:39:23 -0700

    Convert List of elements to string Examples
    Convert List of elements to string Examples

--- R/convert-list-to-string.R
+++ R/convert-list-to-string.R
@@ -1,1 +1,16 @@
-Convert 
+#Convert List of elements to string Examples
+
+# Convert List to string
+li <- list('A','B','C','D','E','F')
+paste(li,collapse = ' ')
+
+
+# Using toString()
+toString(li)
+
+# using do.call()
+do.call(paste, c(li, collapse = " "))
+
+# Load stringi
+library('stringi')
+stri_paste(unlist(li), collapse='')
