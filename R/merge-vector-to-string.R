f0e386295d929a7a9a0575238c286a195a4fba15
NNK <nnkumar13@gmail.com>
2022-07-31 13:03:15 -0700

    R Example of Merge Vector to String
    R Example of Merge Vector to String

---
+++ R/merge-vector-to-string.R
@@ -0,0 +1,28 @@
+# R Example of Merge Vector to String 
+
+# Using paste() function
+v <- c('A','B','C','D')
+print(v)
+print(paste(v,collapse=' '))
+print(paste(v))
+
+# With 2 vectors
+v <- c('A','B','C','D')
+v2 <- c('X','Y','Z')
+print(paste(v,v2,collapse='-'))
+print(paste(v,v2,sep='|'))
+print(paste(v,v2,sep='|',collapse='-'))
+print(paste(v,v2))
+
+# cbin()
+v <- c('A','B','C')
+v2 <- c('X','Y','Z')
+print(cbind(v,v2))
+
+# rbind()
+v <- c('A','B','C')
+v2 <- c('X','Y','Z')
+print(rbind(v,v2))
+
+# do.call()
+do.call(paste, c(as.list(v), sep = " "))
