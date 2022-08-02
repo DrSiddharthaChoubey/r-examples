3ae4222738c3a0bff39892da476d3a598613ef98
NNK <nnkumar13@gmail.com>
2022-08-01 19:53:03 -0700

    Remove duplicates from vector in R
    Remove duplicates from vector in R

---
+++ R/remove-duplicates-in-vector.R
@@ -0,0 +1,24 @@
+# Rempve duplicates from vector in R
+
+# Create vector
+v <- c('A','B','D','C','A','F','G','C','d','E','E')
+
+#Identify Duplicates
+duplicated(v)
+
+# Remove duplicate values
+!duplicated(v)
+v[!duplicated(v)]
+
+# Using unique()
+unique(v)
+
+# To remove contiguous duplicated elements only 
+library(dplyr)
+v <- c('A','A','D','C','C','F','F','C','d','E','E')
+v[v != lag(v)]
+v[v != lag(v, default = v[1])]
+
+# Using union
+v <- c('A','B','D','C','A','F','G','C','d','E','E')
+union(v,v)
