41487aae1af2375d4e867d902be109bc09f7cf27
NNK <nnkumar13@gmail.com>
2022-11-08 21:32:16 -0800

    outer function

--- R/outer-function.R
+++ R/outer-function.R
@@ -1,2 +1,31 @@
 # Outer Function Example
 
+# Outer product of single Vector
+x <- 1:6
+y <- 2
+res <- outer(x,y)
+print(res)
+
+# Outer product of two vectors
+x <- 2:5
+y <- 3:6
+print(x)
+print(y)
+res <- outer(x,y, "+")
+print(res)
+
+# Outer with custom function
+fun1 <- function(x,y){
+  return ((x + y) * 2)
+}
+res2 <- outer(x,y, fun1)
+print(res2)
+
+# Outer product of character vectors
+x <- c('AB','CD')
+y <- c('X','Y')
+fun1 <- function(x,y){
+  return (paste(x, y, sep="_"))
+}
+res2 <- outer(x,y, fun1)
+print(res2)
